7fb94d0196352543ca76c28c3095e923237f0300
Kees Kluskens <kees@webduck.nl>
2019-10-24 13:17:33 +0200

    upgrade to androidx

--- android/src/main/java/com/tuya/smart/rnsdk/utils/TYRCTCommonUtil.java
+++ android/src/main/java/com/tuya/smart/rnsdk/utils/TYRCTCommonUtil.java
@@ -1,6 +1,6 @@
 package com.tuya.smart.rnsdk.utils;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSON;
